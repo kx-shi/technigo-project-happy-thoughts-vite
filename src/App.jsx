c1f155160428e2ccf89feffd20e643c2cc248112
Hannapham1007 <hannaphamm1007@gmail.com>
2023-11-03 11:55:10 +0100

    add textform, handle form submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,57 @@
 import React, { useState, useEffect } from "react";
 import { HeaderComponent } from "./components/HeaderComponent.jsx";
-import { LikeButton } from "./components/LikeButton.jsx"
-import { MessageComponent } from "./components/MessageComponent.jsx"
-import { TextForm } from "./components/TextForm.jsx"
+import { LikeButton } from "./components/LikeButton.jsx";
+import { MessageComponent } from "./components/MessageComponent.jsx";
+import { TextForm } from "./components/TextForm.jsx";
 
 export const App = () => {
   const [thoughts, setThoughts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [newThought, setNewThought] = useState("");
+  const apiUrl = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
 
+  // Fetch initial thoughts through API
+  const fetchThoughts = () => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+    setLoading(false);
+  };
   useEffect(() => {
-    // Fetch initial thoughts through API
-    fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts")
-      .then(res => res.json())
-      .then(json => {
-        setThoughts(json)
-        setLoading(false);
-      });
+    fetchThoughts();
   }, []);
 
   if (loading) {
-    return (
-        <div>Loading...</div>
-    )
+    return <div>Loading...</div>;
   }
+
+  // Handle form submit
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message: newThought }),
+    };
+
+    fetch(apiUrl, options)
+      .then((res) => res.json())
+      .then((updatedThought) => {
+        setThoughts((thought) => [updatedThought, ...thought]);
+      });
+    // After updating a new thought, clear the text-area of the TextForm
+    setNewThought("");
+  };
+  console.log(thoughts);
 
   const handleLikeButtonClick = () => {
     // do something
+  };
 
-  }
-
-  const handleFormSubmit = () => {
+  /* const handleLikeButtonClick = () => {
     // do something
-    
-  }
+  };*/
   /*
   const handleLikeButtonClick = (thoughtID) => {
     // Send the POST request with the id given by likebutton
@@ -42,23 +61,31 @@ export const App = () => {
     })
       .then((res) => res.json())
       .then((updatedThought) => {
+        
         console.log(updatedThought);
         //setThoughts((thoughts) => [updatedThought, ...thoughts])
       })
   }
   */
 
-  console.log(thoughts)
-
-  return(
+  return (
     <>
       <HeaderComponent />
-      <TextForm handleFormSubmitFunc={handleFormSubmit} />
+      <TextForm
+        handleFormSubmit={handleFormSubmit}
+        newThought={newThought}
+        setNewThought={setNewThought}
+      />
       {thoughts.map((thought) => {
-        return(
-          <div key={thought.id} className="thought-container">
-            <MessageComponent props={thought} handleLikeButtonClickFunc={handleLikeButtonClick} />
+        return (
+          <div key={thought._id} className="thought-container">
+            <MessageComponent
+              props={thought}
+              handleLikeButtonClickFunc={handleLikeButtonClick}
+            />
           </div>
-        )})}
+        );
+      })}
     </>
-  )};
+  );
+};
