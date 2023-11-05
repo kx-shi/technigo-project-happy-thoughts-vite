835e0b0f9063951c5665d6430fe6e1394d4e3e02
klara kindlund <>
2023-11-05 14:05:36 +0100

    new css file for likebutton, fixed overflow for time info

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,6 +1,7 @@
 // Component that handles like
 // POST <https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/THOUGHT_ID/like>
 import React, { useState, useEffect } from "react";
+import "../styles/LikeButton.css";
 
 export const LikeButton = ({props, handleLikeButtonClick}) => { 
     return(
